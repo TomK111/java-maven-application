ae9eead66d5082f985772e5c058809d0b21e4bb1
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-07 03:51:29 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,15 +1,20 @@
-def buildApp() {
-    echo 'building the application...'
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
 }
 
-def testApp() {
-    echo 'testing the application...'
+def buildDockerImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
+            usernameVariable: 'USER')]) {
+        sh 'docker build -t 134.122.20.106:8083/java-maven-app:1.4 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin 134.122.20.106:8083"
+        sh 'docker push 134.122.20.106:8083/java-maven-app:1.4'
+    }
 }
 
-def deployApp() {
+def deployDockerImage() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
-
 }
 
 return this
