b8d95a6b0c6d089ef830c18c8cc4d65d08c9848a
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-09 03:28:09 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -18,20 +18,8 @@ def buildDockerImage() {
     }
 }
 
-def loginToNexus() {
-    echo "Logging into Nexus OSS..."
-    withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
-            usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin 134.122.20.106:8083"
-    }
-}
-
 def deployDockerImage() {
     echo 'deploying the application...'
-    withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
-            usernameVariable: 'USER')]) {
-        sh 'docker push 134.122.20.106:8083/java-maven-app:1.4'
-    }
 }
 
 return this
