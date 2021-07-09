4dca5ac07ffa01b7b1ff223aef7f72fe08dbaee2
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-09 09:16:43 +0000

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -13,8 +13,6 @@ def buildDockerImage() {
     withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
             usernameVariable: 'USER')]) {
         sh 'docker build -t 134.122.20.106:8083/java-maven-app:1.5 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin 134.122.20.106:8083"
-        sh 'docker push 134.122.20.106:8083/java-maven-app:1.5'
     }
 }
 
