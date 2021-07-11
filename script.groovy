3b0d91122d8f25db68d0f0156df4ab8733960f45
Tomislav Kraljic <kraljictomislav1@gmail.com>
2021-07-10 21:26:01 -0400

    updated new ip addresses

--- script.groovy
+++ script.groovy
@@ -12,7 +12,7 @@ def buildDockerImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
             usernameVariable: 'USER')]) {
-        sh 'docker build -t 134.122.20.106:8083/java-maven-app:1.5 .'
+        sh 'docker build -t 143.198.166.147:8081/java-maven-app:1.5 .'
     }
 }
 
@@ -21,13 +21,13 @@ def loginToNexus() {
     echo "logging into Nexus OSS..."
     withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
             usernameVariable: 'USER')]) {
-        sh "echo $PASS | docker login -u $USER --password-stdin 134.122.20.106:8083"
+        sh "echo $PASS | docker login -u $USER --password-stdin 143.198.166.147:8081"
     }
 }
 def deployDockerImage() {
     withCredentials([usernamePassword(credentialsId:'nexus-credentials', passwordVariable:'PASS',
             usernameVariable: 'USER')]) {
-        sh 'docker push 134.122.20.106:8083/java-maven-app:1.5'
+        sh 'docker push 143.198.166.147:8081/java-maven-app:1.5'
     }
 }
 
